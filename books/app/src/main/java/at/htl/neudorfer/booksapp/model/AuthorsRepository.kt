5b6370258b9deb20e424e7d7661f812709ea9fa7
NiklasNeudorfer <nikitrash93@gmail.com>
2023-03-10 12:38:40 +0100

    roomdb - added Database access to Repository

--- books/app/src/main/java/at/htl/neudorfer/booksapp/model/AuthorsRepository.kt
+++ books/app/src/main/java/at/htl/neudorfer/booksapp/model/AuthorsRepository.kt
@@ -2,11 +2,32 @@ package at.htl.neudorfer.booksapp.model
 
 import at.htl.neudorfer.booksapp.data.Author
 import at.htl.neudorfer.booksapp.model.api.ApiService
+import at.htl.neudorfer.booksapp.model.db.dao.AuthorDao
+import javax.inject.Inject
 
-data class AuthorsRepository(
-    private val apiService: ApiService = ApiService.getInstance()
+data class AuthorsRepository @Inject constructor(
+    private val authorDao: AuthorDao
 ) {
+    private val apiService: ApiService = ApiService.getInstance()
+
     suspend fun getBookAuthors(): List<Author> {
         return apiService.getAuthors()
     }
+
+
+
+    // ----------------------------------------------------
+    //      DATABASE ACCESS
+    // ----------------------------------------------------
+
+    suspend fun insertAuthor(author: Author) {
+        if (authorDao.findByName(author.fullName) == null) {
+            authorDao.insert(author)
+        }
+    }
+
+    suspend fun deleteAuthor(author: Author) {
+        authorDao.delete(author)
+    }
+
 }
