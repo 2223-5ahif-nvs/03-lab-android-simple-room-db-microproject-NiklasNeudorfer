c614cc50785e9d6760c0f5e59b79f9360ce84b03
NiklasNeudorfer <nikitrash93@gmail.com>
2023-03-10 19:28:37 +0100

    frontend - add title to liked authors list

--- books/app/src/main/java/at/htl/neudorfer/booksapp/ui/favouriteAuthors/FavouriteAuthorsListScreen.kt
+++ books/app/src/main/java/at/htl/neudorfer/booksapp/ui/favouriteAuthors/FavouriteAuthorsListScreen.kt
@@ -1,26 +1,59 @@
 package at.htl.neudorfer.booksapp.ui.favouriteAuthors
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import at.htl.neudorfer.booksapp.data.Author
 
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun FavouriteAuthorsListScreen(viewModel: FavouriteAuthorsViewModel) {
     val authors = viewModel.authors
 
-    LazyColumn() {
+    //val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Magenta)
+
+
+    Text(
+        text = "Your likes:",
+        fontStyle = MaterialTheme.typography.h1.fontStyle,
+        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 30.sp,
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+        /*
+        style = TextStyle(
+            brush = Brush.linearGradient(
+                colors = gradientColors
+            )
+        )
+         */
+    )
+    Divider(
+        startIndent = 8.dp,
+        thickness = 1.dp,
+        color = Color.Gray
+    )
+    LazyColumn {
+
         items(authors) { author ->
             FavouriteAuthorItem(author)
         }
